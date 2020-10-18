3894ac242502baf4f1cc82fcaa58c78cc4223669
vplacido <valentinplacido25@gmail.com>
2020-10-18 19:56:12 -0400

    imported the components to the home container file for testing

--- src/Container/Home.js
+++ src/Container/Home.js
@@ -1,9 +1,15 @@
 import React, {Component} from 'react'
+import NavBar from '../Components/NavBar'
+import Shift from '../Components/Shift'
+import Calendar from '../Components/Calendar'
 
 class Home extends Component {
     render() {
         return (
             <div>
+                <NavBar />
+                <Calendar />
+                <Shift />
 
             </div>
         )

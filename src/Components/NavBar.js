41a6fc8b8ed9bb4ca245d476154e00725f47b715
vplacido <valentinplacido25@gmail.com>
2020-10-25 22:18:21 -0400

    added buttons to navbar for login and signup

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,10 +1,13 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import Login from './Container/Login';
+import SignUp from './/Container/SignUp'
 
 class NavBar extends Component {
     render() {
         return (
             <div>
-
+                <button>Login</button>
+                <button>SignUp</button>
             </div>
         )
     }
